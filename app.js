35a4394e5ebed7305395fa63bc0940bc104fa540
danialouty <daniadaseen@gmail.com>
2026-09-21 22:38:02 +0300

    Refactor user prompts and membership type handling in app.js

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-let name = prompt("please enter your name :");
+let username = prompt("please enter your name :");
 
 let membership = prompt("please enter your mimbership type:");
 
 let membershipType = membership.toLowerCase();
 
+let type = "";
 
+if (membershipType === "student"){
+    type = "Student";
+} 
+else if(membershipType === "regular"){
+    type = "regular";  
+}     
+
+
+
+
+alert ("Hello " + username + " you are a " + type + " member.");
+
+let functional =prompt("do you prefer a fiction or non-fiction book genre");
+
+let titelbook = prompt("please enter your book title:");
+
+
+alert  ("your requested book" +titelbook+"is being reserved");
+
+console.log("dear "+username+" you ordered " + titelbook);
 
