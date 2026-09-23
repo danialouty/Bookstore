a401f322f5819cd92af6a80d1e5d673f47c3fd06
danialouty <daniadaseen@gmail.com>
2026-09-23 15:55:01 +0300

    Enhance user input validation for number prompts and calculate sum of integers

--- loopEx.js
+++ loopEx.js
@@ -32,7 +32,44 @@ console.log(result2);
 
 /////////////////////////////////////
 
+let num =prompt("please enter a number btween 0 and 100: ");
+while(num <0 || num >100 ){
+  num=prompt("enter a number btween 0 and 100");
+
+}
+
+
+///////////////////////////////////////
 
 
 
+let num1 =prompt("please enter a number btween 0 and 100: ");
 
+if(isNaN(num1)===false ){
+while(num1 <0 || num1 >100 ){
+  num1=prompt("enter a number btween 0 and 100");
+
+}
+alert("Thank you! You entered: " + num1);
+}
+else{
+    alert("not a number")
+}
+
+
+
+/////////////////////////////////////////////////////
+
+
+let num3=prompt("please enter a number:");
+let sum=0;
+num3=Number(num3);
+
+for (let i=0 ; i<= num3 ;i++ ){
+
+
+    sum+=i;
+
+}
+
+alert(sum);
